965bd085fa345e5975186d1db7fac1220493ef83
Reynaldo Villarreal <antonios1789@gmail.com>
2016-11-06 13:42:22 -0500

    Update placeholder.py

--- placeholder.py
+++ placeholder.py
@@ -4,15 +4,15 @@ import tensorflow as tf
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
-filename = "/home/rey/Imágenes/original_shack.jpg"    #imagen a extraer los datos altura: 413 y anchura: 622
+filename = "/home/rey/Imágenes/original_shack.jpg"    #imagen a extraer los datos altura: 400 y anchura: 400
 raw_image_data = mpimg.imread(filename)            #
 
 image = tf.placeholder("uint8",[None,None,3])
 #print(raw_image_data)
 slice = tf.slice(image, [0,0,0],[300,400,-1])   
 
-#función slice: image= imagen de entrada, begin=tiene que ser menor que la altura por ejemplo la altura máxima es 413 
-#entonces begin solo puede iniciar con un valor menor a 413 tambien dependiendo del tamaño a cortar, ya que el corte no 
+#función slice: image= imagen de entrada, begin=tiene que ser menor que la altura por ejemplo la altura máxima es 400 
+#entonces begin solo puede iniciar con un valor menor a 400 tambien dependiendo del tamaño a cortar, ya que el corte no 
 #puede ser mayor a la imagen. size = el tamaño resultante del corte, especificado por el usuario.
 
 with tf.Session()as session:
